1c8a926e80d418e52e7fcfe0d9b089ab1dcde69e
Emily Gard <egard8518@vt.edu>
2024-05-24 14:00:50 -0400

    hdfjklgvhsdfkghdv

--- work_lil_b.R
+++ work_lil_b.R
@@ -1,1 +1,8 @@
-work this time you better 
+#work this time you better 
+
+library(tidycensus)
+?census_api_key
+census_api_key(key="5f7550657af93cac63ea0b4d62f4956902da6b50",install = TRUE)
+
+census_api_key(key, overwrite = FALSE, install = TRUE)
+gh repo clone cat-astrophic/stickers
